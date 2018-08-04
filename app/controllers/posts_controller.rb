5fdb153cc97aef9fccd284690d564e80337edb44
Arbind Thakur <arbind.thakur@gmail.com>
2018-08-04 12:34:23 -0700

    adapt responses from redit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,19 @@
+require 'net/http'
+URL = URI.parse('https://www.reddit.com/r/webdev/')
+
 class PostsController < ApplicationController
 
   # GET /posts
   def index
-    @posts = [ 
-      {id: :cool_topic_1, upvoted:false, downvoted: false},
-      {id: :cool_topic_2, upvoted:false, downvoted: false},
-      {id: :cool_topic_3, upvoted:false, downvoted: false}
-    ]
+    puts "HIIII"
+    @posts = RedditAdapter.posts nil
+    puts @posts
+
+    # @posts = [ 
+    #   {id: :cool_topic_1, upvoted:false, downvoted: false},
+    #   {id: :cool_topic_2, upvoted:false, downvoted: false},
+    #   {id: :cool_topic_3, upvoted:false, downvoted: false}
+    # ]
     render json: @posts
   end
 
