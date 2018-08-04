2a8a83fdb6b99d8d866e9c2d6b00eb8020ee9f38
Arbind Thakur <arbind.thakur@gmail.com>
2018-08-04 12:41:11 -0700

    pick up service classes

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module RedditViewer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'services', '{**/}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
