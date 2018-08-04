33f81ad9f2f422024c7043f4e47316c48a5466e6
Arbind Thakur <arbind.thakur@gmail.com>
2018-08-04 16:58:16 -0700

    cleanup extra copy

--- client/src/pages/PostListingsPage.js
+++ client/src/pages/PostListingsPage.js
@@ -13,7 +13,6 @@ class PostListingsPage extends Component {
         </header>
         <PostList/>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
