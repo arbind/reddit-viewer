7da1ef72e1a39af2be1cf834b368426b446dce1a
Arbind Thakur <arbind.thakur@gmail.com>
2018-08-04 12:33:40 -0700

    reddit client

---
+++ app/services/reddit_client.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+REDIT_HOST = "https://www.reddit.com"
+HEADERS = {'User-Agent' => 'reddit_viewer'}
+
+class RedditClient
+  def self.posts(topic='webdev')
+    url = "#{REDIT_HOST}/r/#{topic}.json"
+    response = open(url, {'User-Agent' => 'reddit_viewer'}).read
+    json = JSON.parse(response) || {}
+    # return json
+    data = json["data"] || {}
+    children = data["children"] || []
+    posts = children.map do |c|
+      content = c["data"]
+      {
+        id: content["id"],
+        text: content["selftext"],
+        html: content["selftext_html"]
+      }
+    end
+  end
+end
